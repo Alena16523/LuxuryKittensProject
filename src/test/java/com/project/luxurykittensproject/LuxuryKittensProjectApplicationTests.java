55f6b34e1eea1ddc8253a9bed66e918c00fe2c13
Alena <alenaelhajassaad@yahoo.com>
2023-03-12 12:27:44 -0400

    Initial commit

---
+++ src/test/java/com/project/luxurykittensproject/LuxuryKittensProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.luxurykittensproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuxuryKittensProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
